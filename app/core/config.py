ab9129484ec60c5e989b549535f8a7007589a796
Harold Jefrey Arguello Giraldo <90119278+JeffreyArg@users.noreply.github.com>
2025-09-07 01:30:51 -0500

    feat: Add initial database configuration and session management

---
+++ app/core/config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings:
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
+    PORT: int = int(os.getenv("PORT", 8000))
+    ENV: str = os.getenv("ENV", "development")
+
+settings = Settings()
