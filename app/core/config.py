8b87ea68f82ea19972b5b4a72f5e53d880e79bef
Harold Jefrey Arguello Giraldo <90119278+JeffreyArg@users.noreply.github.com>
2025-09-07 03:36:18 -0500

    refactor: Rename Settings class to Configs and update references in session management

--- app/core/config.py
+++ app/core/config.py
@@ -3,9 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-class Settings:
+class Configs:
     DATABASE_URL: str = os.getenv("DATABASE_URL")
     PORT: int = int(os.getenv("PORT", 8000))
     ENV: str = os.getenv("ENV", "development")
 
-settings = Settings()
+configs = Configs()
